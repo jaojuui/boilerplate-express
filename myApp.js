7360b0b4fad68393176ea2c2881645bafc4a6ac3
jaojuiii <jaojuigraphic2@gmail.com>
2025-09-07 20:38:44 +0700

    update s3

--- myApp.js
+++ myApp.js
@@ -2,11 +2,11 @@ let express = require('express');
 let app = express();
 console.log("Hello World");
 
+path = __dirname + '/views/index.html'
 
 app.get("/", (req, res) => {
-  res.send("Hello Express");
+  res.sendFile(path);
 });
-
 
 
 
