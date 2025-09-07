0bf6adf5d04ecd2a78b58bf84b949222516be530
jaojuiii <jaojuigraphic2@gmail.com>
2025-09-07 22:04:10 +0700

    update s10

--- myApp.js
+++ myApp.js
@@ -32,6 +32,10 @@ app.get("/now", (req,res,next)=>{
 app.get("/:word/echo",(req,res)=>{
     res.json({echo: req.params.word});
 })
+app.get("/name",(req,res)=>{
+    res.json({name:`${req.query.first} ${req.query.last}`});
+})
+app.route(path).get(handler).post(handler);
 
 
  module.exports = app;
