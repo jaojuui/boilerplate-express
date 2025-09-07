8c4f54eab2cd84a92a1448e284f53d61921f654b
jaojuiii <jaojuigraphic2@gmail.com>
2025-09-07 21:20:25 +0700

    update

--- myApp.js
+++ myApp.js
@@ -14,16 +14,12 @@ app.get("/", (req, res) => {
 app.use('/public', express.static(pathPublic));
 
 app.get("/json", (req, res) => {
-  let message = "Hello json";
-
-  if (process.env.MESSAGE_STYLE === "uppercase") {
-    message = message.toUpperCase();
-  }
-
-  res.json({ message: message });
+    if(process.env.MESSAGE_STYLE=="uppercase"){
+        res.json({"message": "HELLO JSON"});
+    }else{
+        res.json({"message": "Hello json"});
+    }
 });
-
-module.exports = app;
 
 
 
