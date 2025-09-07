eb79919c08d94238cc56e1923e529088068b0464
jaojuiii <jaojuigraphic2@gmail.com>
2025-09-07 19:58:29 +0700

    update edit s1

--- myApp.js
+++ myApp.js
@@ -1,9 +1,10 @@
 console.log("Hello World");
-let express = require('express');
+
+let express = require('express'); 
 let app = express();
 
-
-
+// ต้องมีบรรทัดนี้ด้วยเพื่อให้ระบบตรวจ app ได้
+module.exports = app;
 
 
 
