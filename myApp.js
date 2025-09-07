0566fa8c783d0e88c619139f6f8a892742b89b0c
jaojuiii <jaojuigraphic2@gmail.com>
2025-09-07 21:24:26 +0700

    update s6

--- myApp.js
+++ myApp.js
@@ -14,30 +14,15 @@ app.get("/", (req, res) => {
 app.use('/public', express.static(pathPublic));
 
 app.get("/json", (req, res) => {
-    if(process.env.MESSAGE_STYLE=="uppercase"){
-        res.json({"message": "HELLO JSON"});
-    }else{
-        res.json({"message": "Hello json"});
-    }
+  let message = "Hello json";
+
+  // ต้องอ่านค่าจาก process.env **ภายใน** route handler!
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    message = message.toUpperCase();
+  }
+
+  res.json({ message: message });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
